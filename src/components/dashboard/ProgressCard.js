08b0342acc5c66f551a67b66b313a2f63a5d3e97
creme332 <65414576+creme332@users.noreply.github.com>
2023-06-23 15:57:05 +0400

    add progress card component

--- src/components/dashboard/ProgressCard.js
+++ src/components/dashboard/ProgressCard.js
@@ -1,22 +1,27 @@
-import { Text, Progress, Card } from '@mantine/core';
+import { Text, Progress, Card } from "@mantine/core";
 
-export function ProgressCard() {
+export function ProgressCard({
+  label = "test",
+  description = "Rs 5.431 / 10.000",
+  value = 26,
+}) {
   return (
     <Card
       withBorder
       radius="md"
       padding="xl"
       sx={(theme) => ({
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+        backgroundColor:
+          theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
       })}
     >
       <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
-        Monthly goal
+        {label}
       </Text>
       <Text fz="lg" fw={500}>
-        $5.431 / $10.000
+        {description}
       </Text>
-      <Progress value={54.31} mt="md" size="lg" radius="xl" />
+      <Progress value={value} mt="md" size="lg" radius="xl" />
     </Card>
   );
 }
