abe85ab7e366a353345a8102ef52562005b51a8b
creme332 <65414576+creme332@users.noreply.github.com>
2023-06-23 09:55:00 +0400

    add icons and set up mantine

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,42 @@
-import '@/styles/globals.css'
+import { MantineProvider, ColorSchemeProvider, Container } from "@mantine/core";
+import { useState } from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // states for theme
+  const [colorScheme, setColorScheme] = useState("dark");
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  return (
+    <>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          {" "}
+          <Head>
+            <meta charSet="utf-8" />
+            <link
+              rel="icon"
+              href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😀</text></svg>"
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <meta name="description" content="description of website here" />
+            <title>qkwiqq</title>
+          </Head>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
